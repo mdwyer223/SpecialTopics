a1c6da630c1507497aa8bb703ab37302e75c088a
Matthew Dwyer <mdwyer223@aol.com>
2014-02-07 12:52:38 -0500

    Added more images and directories for organization

---
+++ AdlezHolder/AdlezHolder/AdlezHolder/Data/Dungeons/Dungeon2/TopLeft.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace AdlezHolder
+{
+    public class TopLeft : MapDataHolder
+    {
+        public TopLeft(Character player)
+            : base(player)
+        {
+            background = Game1.GameContent.Load<Texture2D>("BackgroundsAndFloors/Dungeons/Dungeon2/Top");
+            backgroundRec = new Rectangle(0, 0, background.Width, background.Height);
+
+            updateCorners();
+
+            //Top
+            Wall wall = new Wall(new Rectangle(148, 215 - backgroundRec.Height, backgroundRec.Width, backgroundRec.Height),
+                Game1.GameContent.Load<Texture2D>("Random/The best thing ever"), new Vector2(130, 215 - backgroundRec.Height));
+            addImmovable(wall);
+
+            //Bottom
+            wall = new Wall(new Rectangle(148, backgroundRec.Height, backgroundRec.Width, backgroundRec.Width),
+                Game1.GameContent.Load<Texture2D>("Random/The best thing ever"), new Vector2(129 - backgroundRec.Width, 215));
+            addImmovable(wall);
+
+            //Right
+            wall = new Wall(new Rectangle(backgroundRec.Width - 148, 215, backgroundRec.Width, backgroundRec.Height),
+                Game1.GameContent.Load<Texture2D>("Random/The best thing ever"), new Vector2(852, 216));
+            addImmovable(wall);
+
+            //Left
+            wall = new Wall(new Rectangle(148 - backgroundRec.Width, 215, backgroundRec.Width, backgroundRec.Height),
+                Game1.GameContent.Load<Texture2D>("Random/The best thing ever"), new Vector2(130, backgroundRec.Height));
+            addImmovable(wall);
+        }
+    }
+}
